ee54e06276b8cdd8e0bc382b39a6aba45d51d9eb
java-oop <topsh90@gmail.com>
2024-03-18 10:53:07 +0900

    first commit

--- src/dependencyoff/ABMain.java
+++ src/dependencyoff/ABMain.java
@@ -2,13 +2,9 @@ package dependencyoff;
 
 public class ABMain {
     public static void main(String[] args) {
-        A a = new A("aClass");
+        A a = new A("aField");
         System.out.println(a.aField);
         System.out.println(a.aMethod());
-
-        B b = new B("bClass");
-        System.out.println(b.bField);
-        System.out.println(b.bMethod());
 
     }
 
@@ -19,16 +15,8 @@ class A{
     public A(String aField){
         this.aField = aField;
     }
-    class B{
-        String bField;
-        public B(String bField){
-            this.bField = bField;
-        }
-    }
+
     String aMethod(){
     return "A.aMethod";
-    }
-    String bMethod(){
-        return "B.bMethod";
     }
 }
